0f3a475a09db2b0cde35692398a012de0bd80e59
xies <xies@stanford.edu>
2018-05-08 16:46:35 -0700

    Produced CDT1 and the GPI candidates

--- pull_target_seqs.py
+++ pull_target_seqs.py
@@ -9,9 +9,12 @@ Created on Thu May  3 18:12:37 2018
 import numpy as np
 import scipy as sp
 import uniprot
-from Bio import SeqIO, AlignIO
+from Bio import SeqIO, AlignIO, motifs, Alphabet
+from Bio.Seq import Seq
 import pandas as pd
 import seaborn as sb
+import matplotlib.pyplot as plt
+import os
 
 # Load hand-curated CycD putative target list
 filename = '/data/cycd_targets/cycd_target_uniprot.txt'
@@ -24,5 +27,30 @@ upData = uniprot.batch_uniprot_metadata(entries, 'cache')
 uniprot.write_fasta(out_name, upData, entries)
 
 split_fastas(out_name)
+
+
+
+PSIPRED_DIR = '/data/cycd_targets/cycd_target_uniprot_individuals'
+seqs = []
+
+for filename in os.listdir(PSIPRED_DIR):
+    if filename.endswith('.ss2'):
+        print 'Working on ', filename
+        
+        #Load PSIPRED VFORMAT in a sane way to extract only relevant info
+        df = pd.read_csv(os.path.join(PSIPRED_DIR,filename),header=0, delim_whitespace=True,skiprows=0,
+                         names=['position','AA','prediction'],usecols=[0,1,2], index_col=0)
+        fastaname,ext = os.path.splitext(filename)
+        s = SeqIO.read(os.path.join(PSIPRED_DIR,fastaname),'fasta')
+        
+        s.seq = Seq(''.join(df.prediction))
+        seqs.append(s)
+        
+    else:
+        continue
+
+SeqIO.write(seqs,os.path.join( PSIPRED_DIR,'psipred.fasta' ),'fasta')
+
+
 
 
